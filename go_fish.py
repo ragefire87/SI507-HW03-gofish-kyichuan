4d1755e9d78abb00f46ec54dd54a7d4e6114ede6
Yichuan <kerhyichuan@hotmail.com>
2018-09-20 16:30:42 -0400

    add go_fish

---
+++ go_fish.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 20 14:46:14 2018
+
+@author: Yichuan
+"""
+
+class Card(object):
+	suit_names =  ["Diamonds","Clubs","Hearts","Spades"]
+	rank_levels = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+	faces = {1:"Ace",11:"Jack",12:"Queen",13:"King"}
+
+	def __init__(self, suit=0,rank=2):
+		self.suit = self.suit_names[suit]
+		if rank in self.faces: # self.rank handles printed representation
+			self.rank = self.faces[rank]
+
+		else:
+			self.rank = rank
+		self.rank_num = rank # To handle winning comparison
+
+	def __str__(self):
+		return "{} of {}".format(self.rank,self.suit)
+
+class Deck(object):
+	def __init__(self): # Don't need any input to create a deck of cards
+		# This working depends on Card class existing above
+		self.cards = []
+		for suit in range(4):
+			for rank in range(1,14):
+				card = Card(suit,rank)
+				self.cards.append(card) # appends in a sorted order
+
+	def __str__(self):
+		total = []
+		for card in self.cards:
+			total.append(card.__str__())
+		# shows up in whatever order the cards are in
+		return "\n".join(total) # returns a multi-line string listing each card
+
+	def pop_card(self, i=-1): 
+		# removes and returns a card from the Deck
+		# default is the last card in the Deck
+		return self.cards.pop(i) # this card is no longer in the deck -- taken off
+
+	def shuffle(self):
+		random.shuffle(self.cards)
+
+	def replace_card(self, card): 
+		card_strs = [] # forming an empty list
+		for c in self.cards: # each card in self.cards (the initial list)
+			card_strs.append(c.__str__()) # appends the string that represents that card to the empty list
+		if card.__str__() not in card_strs: # if the string representing this card is not in the list already
+			self.cards.append(card) # append it to the list
+
+	def sort_cards(self):
+		# Basically, remake the deck in a sorted way
+		# This is assuming you cannot have more than the normal 52 cars in a deck
+		self.cards = []
+		for suit in range(4):
+			for rank in range(1,14):
+				card = Card(suit,rank)
+				self.cards.append(card)
+				
+	def play_game(self):
+	
+		self.player1 = []
+		self.player2 = []
+		self.player3 = []
+		card = Card()
+		deck = Deck()
+	
+		for i in range(7):
+			self.player1.append(deck.pop_card())
+			self.player2.append(deck.pop_card())
+			self.player3.append(deck.pop_card())
+			print (self.player1)
+
+
+		
